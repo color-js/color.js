9292d64d1a669af4e1e6b6313a185e27d27995a1
Lea Verou <lea@verou.me>
2020-05-08 13:31:39 -0400

    Make gradient dynamic

--- js/index.js
+++ js/index.js
@@ -16,3 +16,27 @@ $$("pre.runnable > code").forEach(code => {
 		inside: pre
 	});
 });
+
+let root = document.documentElement;
+let cs = getComputedStyle(root);
+let colors = {
+	red: new Color(cs.getPropertyValue("--color-red")),
+	green: new Color(cs.getPropertyValue("--color-green")),
+	blue: new Color(cs.getPropertyValue("--color-blue"))
+};
+
+let supportsP3 = window.CSS && CSS.supports("color", "color(display-p3 0 1 0)");
+let interpolationOptions = {steps: 5, space: "lch", outputSpace: supportsP3? "p3" : "hsl"};
+
+
+
+let vars = {
+	"gradient-steps": [
+		...colors.red.steps(colors.green, interpolationOptions),
+		...colors.green.steps(colors.blue, interpolationOptions),
+		...colors.blue.steps(colors.red, interpolationOptions)
+	]
+};
+
+let sheet = $.create("style", {inside: document.head}).sheet;
+sheet.insertRule(`:root {${Object.entries(vars).map((name, value) => `--color-${name}: ${value}`)}}`);
