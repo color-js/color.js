5653facd2b426fceedad054c997f297a71734e6c
Lea Verou <lea@verou.me>
2020-05-12 12:03:57 -0400

    Import hwb in ./color.js

--- color.js
+++ color.js
@@ -1,11 +1,16 @@
+// Import all modules of Color.js and assign Color to a global,
+// for easier testing and experimentation
 import Color, {util} from "./src/color.js";
+
 import "./src/spaces/lab-lch.js";
 import "./src/spaces/srgb.js";
 import "./src/spaces/hsl.js";
+import "./src/spaces/hwb.js";
 import "./src/spaces/p3.js";
 import "./src/spaces/a98rgb.js";
 import "./src/spaces/prophoto.js";
 import "./src/spaces/rec2020.js";
+
 import "./src/interpolation.js";
 
 window.Color = window.Color || Color;
