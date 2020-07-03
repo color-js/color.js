d9331830f84d7c84ac0ebab3be6783bda625b904
Chris Lilley <chris@w3.org>
2020-07-03 13:33:19 -0400

    [spaces/absxyzd65] less copy paste

--- src/spaces/absxyzd65.js
+++ src/spaces/absxyzd65.js
@@ -6,7 +6,7 @@ Color.defineSpace({
 // SDR spaces are converted per BT.2048
 // so that diffuse, media white is 203 cd/m²
     id: "absxyzd65",
-    name: "AbsXYZD65",
+    name: "Absolute XYZ D65",
 	coords: {
 		Xa: [0, 9504.7],
 		Ya: [0, 10000],
@@ -21,21 +21,8 @@ Color.defineSpace({
 
 		const W1 = Color.whites.D50;
 		const W2 = Color.whites.D65;
-        let env = { W1, W2, XYZ, options};
 
-		Color.hooks.run("chromatic-adaptation-start", env);
-
-		if (!env.M) {
-			env.M = [
-				[ 0.9555766, -0.0230393,  0.0631636],
-				[-0.0282895,  1.0099416,  0.0210077],
-				[ 0.0122982, -0.0204830,  1.3299098]
-			];
-		}
-
-		Color.hooks.run("chromatic-adaptation-end", env);
-
-		XYZ = util.multiplyMatrices(env.M, env.XYZ);
+		XYZ = Color.chromaticAdaptation(W1, W2, XYZ);
 
 		const {Yw} = this;
 
@@ -43,11 +30,9 @@ Color.defineSpace({
 		// Maximum luminance in PQ is 10,000 cd/m²
         // Relative XYZ has Y=1 for media white
 
-        let AbsXYZ = XYZ.map (function (val) {
+        return XYZ.map (function (val) {
 			return Math.max(val * Yw, 0);
 		});
-
-		return AbsXYZ;
     },
     toXYZ (AbsXYZ) {
 
@@ -58,23 +43,9 @@ Color.defineSpace({
         });
 
 		// Then adapt to D50
-
 		const W1 = Color.whites.D65;
 		const W2 = Color.whites.D50;
-        let env = { W1, W2, XYZ, options};
+		return Color.chromaticAdaptation(W1, W2, AbsXYZ)
 
-		Color.hooks.run("chromatic-adaptation-start", env);
-
-		if (!env.M) {
-			env.M = [
-				[ 1.0478112,  0.0228866, -0.0501270],
-				[ 0.0295424,  0.9904844, -0.0170491],
-				[-0.0092345,  0.0150436,  0.7521316]
-		];
-	}
-
-	Color.hooks.run("chromatic-adaptation-end", env);
-
-	return util.multiplyMatrices(env.M, env.XYZ);
 	}
 });
