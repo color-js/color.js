9da00fde685f8274f3b7ffd063a5636b9bcac0f9
Lea Verou <lea@verou.me>
2020-07-08 17:59:04 -0400

    Do not run index.js color.js code when debugging

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,10 @@ let $$ = $.$;
 
 import {} from "../../notebook/color-notebook.js";
 
+if (location.pathname.indexOf("/docs/") > -1 && window.toc) {
+	import("./docs.js");
+}
+
 let root = document.documentElement;
 let colors = {
 	red: new Color("--color-red"),
@@ -12,45 +16,44 @@ let colors = {
 
 let supportsP3 = window.CSS && CSS.supports("color", "color(display-p3 0 1 0)");
 let interpolationOptions = {steps: 5, space: "lch", outputSpace: supportsP3? "p3" : "hsl"};
-let redGreen = colors.red.range(colors.green, interpolationOptions);
-let greenBlue = colors.green.range(colors.blue, interpolationOptions);
-let blueRed = colors.blue.range(colors.red, interpolationOptions);
 
-let vars = {
-	"gradient-steps": [
-		...Color.steps(redGreen, interpolationOptions),
-		...Color.steps(greenBlue, interpolationOptions),
-		...Color.steps(blueRed, interpolationOptions)
-	],
-	"color-red-light": colors.red.clone().set({lightness: 80}),
-	"color-green-light": colors.green.clone().set({lightness: 80}),
-	"color-blue-light": colors.blue.clone().set({lightness: 80}),
+if (!Color.DEBUGGING) {
+	let redGreen = colors.red.range(colors.green, interpolationOptions);
+	let greenBlue = colors.green.range(colors.blue, interpolationOptions);
+	let blueRed = colors.blue.range(colors.red, interpolationOptions);
 
-	"color-red-lighter": colors.red.clone().set({lightness: 94}),
-	"color-green-lighter": colors.green.clone().set({lightness: 95}),
-	"color-blue-lighter": colors.blue.clone().set({lightness: 94}),
+	let vars = {
+		"gradient-steps": [
+			...Color.steps(redGreen, interpolationOptions),
+			...Color.steps(greenBlue, interpolationOptions),
+			...Color.steps(blueRed, interpolationOptions)
+		],
+		"color-red-light": colors.red.clone().set({lightness: 80}),
+		"color-green-light": colors.green.clone().set({lightness: 80}),
+		"color-blue-light": colors.blue.clone().set({lightness: 80}),
 
-	"color-red-green": redGreen(.5),
-	"color-green-blue": greenBlue(.5),
-	"color-blue-red": blueRed(.5),
+		"color-red-lighter": colors.red.clone().set({lightness: 94}),
+		"color-green-lighter": colors.green.clone().set({lightness: 95}),
+		"color-blue-lighter": colors.blue.clone().set({lightness: 94}),
 
-	"color-red-green-light": redGreen(.5).set({lightness: 94}),
-	"color-green-blue-light": greenBlue(.5).set({lightness: 94}),
-	"color-blue-red-light": blueRed(.5).set({lightness: 94}),
-};
+		"color-red-green": redGreen(.5),
+		"color-green-blue": greenBlue(.5),
+		"color-blue-red": blueRed(.5),
 
-$.create("style", {
-	inside: document.head,
-	textContent: `:root {
-		${Object.entries(vars).map(pair => `--${pair[0]}: ${pair[1]}`).join(";\n")};
-		--scrolltop: ${root.scrollTop};
-	}`
-});
+		"color-red-green-light": redGreen(.5).set({lightness: 94}),
+		"color-green-blue-light": greenBlue(.5).set({lightness: 94}),
+		"color-blue-red-light": blueRed(.5).set({lightness: 94}),
+	};
+
+	$.create("style", {
+		inside: document.head,
+		textContent: `:root {
+			${Object.entries(vars).map(pair => `--${pair[0]}: ${pair[1]}`).join(";\n")};
+			--scrolltop: ${root.scrollTop};
+		}`
+	});
+}
 
 document.addEventListener("scroll", evt => {
 	root.style.setProperty("--scrolltop", root.scrollTop);
 }, {passive: true});
-
-if (location.pathname.indexOf("/docs/") > -1 && window.toc) {
-	import("./docs.js");
-}
