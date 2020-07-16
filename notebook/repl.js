0d3a500ce6f85bfa4bee64df8a79d333af3422e9
Lea Verou <lea@verou.me>
2020-07-15 20:30:33 -0400

    Update Markdown when saving too

--- notebook/repl.js
+++ notebook/repl.js
@@ -11,29 +11,37 @@ document.addEventListener("mv-markdown-render", function(evt) {
 	});
 });
 
+function updateMarkdown() {
+	// Update code snippets with actual contents
+	let node = Mavo.all.colorNotebook.root.children.content;
+	let value = node.value;
+	// This approach will fail when a) we have duplicate code in multiple snippets
+	// b) when we have empty code areas
+
+	for (let notebook of Notebook.all) {
+		if (notebook?.edited) {
+			value = value.replace("```js\n" + notebook.initialCode + "\n```", "```js\n" + notebook.code + "\n```");
+		}
+
+		notebook.destroy();
+	}
+
+	if (node.value !== value
+		&& confirm("You have edited the code snippets, do you want to transfer these changes to your Markdown?")) {
+		node.value = value;
+	}
+}
+
+Mavo.hooks.add("save-start", function() {
+	if (this.id === "colorNotebook") {
+		updateMarkdown();
+	}
+});
+
 
 let editObserver = new Mavo.Observer(container, "mv-mode", () => {
 	if (container.getAttribute("mv-mode") === "edit") {
-		// TODO ask whether to sync changes
-		// Update code snippets with actual contents
-		let node = Mavo.Node.get(container);
-		let value = node.value;
-		// This approach will fail when a) we have duplicate code in multiple snippets
-		// b) when we have empty code areas
-
-		for (let notebook of Notebook.all) {
-			if (notebook?.edited) {
-				value = value.replace("```js\n" + notebook.initialCode + "\n```", "```js\n" + notebook.code + "\n```");
-			}
-
-			notebook.destroy();
-		}
-
-		if (node.value !== value
-			&& confirm("You have edited the code snippets, do you want to transfer these changes to your Markdown?")) {
-			node.value = value;
-		}
-
+		updateMarkdown();
 	}
 });
 
