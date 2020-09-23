c2f2952c42ef0c7799c2131d08bb53fd1dfe1193
Chris Lilley <chris@w3.org>
2020-09-23 18:01:13 +0300

    [spaces/ictcp] more tidying & commenting

--- src/spaces/ictcp.js
+++ src/spaces/ictcp.js
@@ -36,14 +36,16 @@ Color.defineSpace({
 	m2: 2523 / 32,
 	im1: 16384 / 2610,
 	im2: 32 / 2523,
-	// The matrix below includes the crosstalk components
+	// The matrix below includes the 4% crosstalk components
+	// and is from the Dolby "What is ICtCp" paper"
 	XYZtoLMS_M: [
 		[ 0.3592,  0.6976, -0.0358],
 		[-0.1922,  1.1004,  0.0755],
 		[ 0.0070,  0.0749,  0.8434]
 	],
 	// linear-light Rec.2020 to LMS, again with crosstalk
-	// rational terms from Froehlich p.97
+	// rational terms from Jan Fröhlich,
+	// Encoding High Dynamic Range andWide Color Gamut Imagery, p.97
 	Rec2020toLMS_M: [
 		[ 1688 / 4096,  2146 / 4096,   262 / 4096 ],
 		[  683 / 4096,  2951 / 4096,   462 / 4096 ],
@@ -51,13 +53,13 @@ Color.defineSpace({
 	],
 	// this includes the Ebner LMS coefficients,
 	// the rotation, and the scaling to [-0.5,0.5] range
-	// rational terms from Froehlich p.97
+	// rational terms from Fröhlich p.97
 	LMStoIPT_M: [
 		[  2048 / 4096,   2048 / 4096,       0      ],
 		[  6610 / 4096, -13613 / 4096,  7003 / 4096 ],
 		[ 17933 / 4096, -17390 / 4096,  -543 / 4096 ]
 	],
-	// inverted matrices
+	// inverted matrices, calculated from the above
 	IPTtoLMS_M: [
 		[0.99998889656284013833, 0.00860505014728705821,  0.1110343715986164786 ],
 		[1.0000111034371598616, -0.00860505014728705821, -0.1110343715986164786 ],
