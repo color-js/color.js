82527c19dff2b8c4fe55c2f162b2527944651067
Christoph Päper <bugzilla@crissov.de>
2021-06-19 16:59:48 +0200

    reinstate trailing zeros

--- src/CATs.js
+++ src/CATs.js
@@ -31,9 +31,9 @@ Color.adapt = function (W1, W2, id = "Bradford") {
 
 	// all practical illuminants have non-zero XYZ so no division by zero can occur below
 	let scale = [
-		[ρd/ρs,    0,      0      ],
-		[0,        γd/γs,  0      ],
-		[0,        0,      βd/βs  ]
+		[ ρd/ρs,      0.0000000,  0.0000000 ],
+		[ 0.0000000,  γd/γs,      0.0000000 ],
+		[ 0.0000000,  0.0000000,  βd/βs     ]
 	];
 	// console.log({scale});
 
@@ -46,14 +46,14 @@ Color.adapt = function (W1, W2, id = "Bradford") {
 Color.defineCAT({
 	id: "von Kries",
 	toCone_M: [
-		[  0.40024,    0.7076,    -0.08081   ],
-		[ -0.2263,     1.16532,    0.0457    ],
-		[  0.0,        0.0,        0.91822   ]
+		[  0.4002400,  0.7076000, -0.0808100 ],
+		[ -0.2263000,  1.1653200,  0.0457000 ],
+		[  0.0000000,  0.0000000,  0.9182200 ]
 	],
 	fromCone_M: [
 		[  1.8599364, -1.1293816,  0.2198974 ],
 		[  0.3611914,  0.6388125, -0.0000064 ],
-		[  0.0,        0.0,        1.0890636 ]
+		[  0.0000000,  0.0000000,  1.0890636 ]
 	]
 });
 Color.defineCAT({
@@ -61,9 +61,9 @@ Color.defineCAT({
 	// Convert an array of XYZ values in the range 0.0 - 1.0
 	// to cone fundamentals
 	toCone_M: [
-		[  0.8951,     0.2664,    -0.1614    ],
-		[ -0.7502,     1.7135,     0.0367    ],
-		[  0.0389,    -0.0685,     1.0296    ]
+		[  0.8951000,  0.2664000, -0.1614000 ],
+		[ -0.7502000,  1.7135000,  0.0367000 ],
+		[  0.0389000, -0.0685000,  1.0296000 ]
 	],
 	// and back
 	fromCone_M: [
@@ -91,9 +91,9 @@ Color.defineCAT({
 Color.defineCAT({
 	id: "CAT16",
 	toCone_M: [
-		[  0.401288,  0.650173, -0.051461 ],
-		[ -0.250268,  1.204414,  0.045854 ],
-		[ -0.002079,  0.048952,  0.953127 ]
+		[  0.4012880,  0.6501730, -0.0514610 ],
+		[ -0.2502680,  1.2044140,  0.0458540 ],
+		[ -0.0020790,  0.0489520,  0.9531270 ]
 	],
 	// the extra precision is needed to avoid roundtripping errors
 	fromCone_M: [
@@ -107,22 +107,22 @@ Object.assign(Color.whites, {
 	// whitepoint values from ASTM E308-01 with 10nm spacing, 1931 2 degree observer
 	// all normalized to Y (luminance) = 1.00000
 	// Illuminant A is a tungsten electric light, giving a very warm, orange light.
-	A:  [1.09850,  1.0,     0.35585],
+	A:   [ 1.09850,  1.00000,  0.35585 ],
 
 	// Illuminant C was an early approximation to daylight: illuminant A with a blue filter.
-	C:  [0.98074,  1.0,     1.18232],
+	C:   [ 0.98074,  1.00000,  1.18232 ],
 
 	// The daylight series of illuminants simulate natural daylight.
 	// The color temperature (in degrees Kelvin/100) ranges from
 	// cool, overcast daylight (D50) to bright, direct sunlight (D65).
-	D55: [0.95682, 1.0,     0.92149],
-	D75: [0.94972, 1.0,     1.22638],
+	D55: [ 0.95682,  1.00000,  0.92149 ],
+	D75: [ 0.94972,  1.00000,  1.22638 ],
 
 	// Equal-energy illuminant, used in two-stage CAT16
-	E:   [1.0,     1.0,     1.0    ],
+	E:   [ 1.00000,  1.00000,  1.00000 ],
 
 	// The F series of illuminants represent flourescent lights
-	F2:  [0.99186, 1.0,     0.67393],
-	F7:  [0.95041, 1.0,     1.08747],
-	F11: [1.00962, 1.0,     0.64350]
+	F2:  [ 0.99186,  1.00000,  0.67393 ],
+	F7:  [ 0.95041,  1.00000,  1.08747 ],
+	F11: [ 1.00962,  1.00000,  0.64350 ]
 });
