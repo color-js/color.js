3729c3827f36e6e911ce6cccb02132355f35b039
Lea Verou <lea@verou.me>
2022-06-19 19:16:42 +0300

    Use X, Y, Z names for XYZ coords, ditch array syntax for coords

--- src/spaces/xyz-d65.js
+++ src/spaces/xyz-d65.js
@@ -3,7 +3,11 @@ import ColorSpace from "../space.js";
 const space = ColorSpace.create({
 	id: "xyz-d65",
 	name: "XYZ D65",
-	coords: ["x", "y", "z"],
+	coords: {
+		x: {name: "X"},
+		y: {name: "Y"},
+		z: {name: "Z"},
+	},
 	white: "D65"
 });
 
