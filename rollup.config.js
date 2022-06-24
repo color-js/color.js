7f23a06c7970956e1fe9ff293a2a46e9651d13f3
Lea Verou <lea@verou.me>
2022-06-24 12:50:28 +0300

    Bundle fixes, closes #155
    - Use .cjs for CJS bundles
    - Default to ESM (color.js points to ESM bundle, color.iife.js is the IIFE version)
    - Simplify Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,49 +1,41 @@
 import { terser } from "rollup-plugin-terser";
 
-function bundle(format, {minify} = {}) {
-	let filename = "color";
-	let options = {
-		format: format,
-		sourcemap: true,
-		plugins: []
-	};
+let bundles = [
+	{
+		"file": "dist/color.iife.js",
+		"format": "iife",
+		"sourcemap": true,
+		"name": "Color"
+	},
+	{
+		"file": "dist/color.js",
+		"format": "esm",
+		"sourcemap": true,
+	},
+	{
+		"file": "dist/color.cjs",
+		"format": "cjs",
+		"sourcemap": true,
+		"exports": "named",
+	},
+];
 
-	if (format === "iife") {
-		options.name = "Color";
-	}
-	else {
-		filename += "." + format;
+// Add minified versions of every bundle
+bundles = bundles.flatMap(bundle => {
+	let minBundle = Object.assign({}, bundle);
+	minBundle.file = minBundle.file.replace(/\.\w+$/, ".min$&");
+	minBundle.plugins ||= [];
+	minBundle.plugins.push(terser({
+		compress: true,
+		mangle: true
+	}));
 
-		if (format === "cjs") {
-			options.exports = "named"; /** Disable warning for default imports */
-		}
-	}
-
-	if (minify) {
-		filename += ".min";
-
-		options.plugins.push(terser({
-			compress: true,
-			mangle: true
-		}));
-	}
-
-	return {
-		file: `dist/${filename}.js`,
-		...options
-	};
-}
+	return [bundle, minBundle];
+});
 
 export default {
 	input: "src/main.js",
-	output: [
-		bundle("iife"),
-		bundle("iife", {minify: true}),
-		bundle("esm"),
-		bundle("esm", {minify: true}),
-		bundle("cjs"),
-		bundle("cjs", {minify: true})
-	],
+	output: bundles,
 	onwarn (warning, rollupWarn) {
 		if (warning.code !== 'CIRCULAR_DEPENDENCY') {
 			rollupWarn(warning);
