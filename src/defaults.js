6bcd753aca657aa513520c120e2f429d3b04f87e
Lea Verou <lea@verou.me>
2022-06-26 16:03:06 +0300

    Update defaults.js

--- src/defaults.js
+++ src/defaults.js
@@ -2,7 +2,7 @@
 const hasDOM = typeof document !== "undefined";
 
 export default {
-	gamutMapping: "lch.c",
+	gamut_mapping: "lch.c",
 	precision: 5,
 	deltaE: "76", // Default deltaE method
 	css_space: "srgb", // Default space for CSS output. Code in Color.js makes this wider if there's a DOM available
