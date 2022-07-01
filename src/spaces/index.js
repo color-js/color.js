fd08b6db89ec70184af31d01429fb0b4563c4151
Sérgio Gomes <mail@sgomes.com>
2022-07-01 14:06:20 +0100

    Formatting fixes

--- src/spaces/index.js
+++ src/spaces/index.js
@@ -3,7 +3,7 @@ import ColorSpace from "../space.js";
 import * as spaces from './index-fn.js';
 
 for (let key of Object.keys(spaces)) {
-  ColorSpace.register(spaces[key]);
+	ColorSpace.register(spaces[key]);
 }
 
 // ...
