e726c715364aac4a4029a61b2438e299af416695
Sérgio Gomes <mail@sgomes.com>
2022-07-04 13:35:11 +0100

    Export spaces in spaces/index.js

--- src/spaces/index.js
+++ src/spaces/index.js
@@ -1,9 +1,8 @@
 import ColorSpace from "../space.js";
-
 import * as spaces from './index-fn.js';
+
+export * as spaces from './index-fn.js';
 
 for (let key of Object.keys(spaces)) {
 	ColorSpace.register(spaces[key]);
 }
-
-// ...
