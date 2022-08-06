3da420f6d1c892b965c343bbbb04aa812b5ab47f
Lea Verou <lea@verou.me>
2022-08-06 18:09:53 +0300

    blackwhite demo

--- apps/blackwhite/index.js
+++ apps/blackwhite/index.js
@@ -30,6 +30,7 @@ function computeTextColor() {
 }
 
 function drawColors() {
+	colors.innerHTML = "";
 	let granularity = Math.cbrt(number_of_colors.value);
 	// root.style.setProperty("--granularity", granularity);
 	let increment = 1 / granularity;
@@ -38,9 +39,14 @@ function drawColors() {
 		for (let g=0; g<=1; g += increment) {
 			for (let b=0; b<=1; b += increment) {
 				let color = new Color("srgb", [r, g, b]);
+				let [l, c, h] = color.getAll("oklch");
+				l = Math.round(l * 100);
+				c = Math.round(c * 100);
+				h = Math.round(h);
+
 				let div = document.createElement("div");
 				div.textContent = "Text";
-				div.style.backgroundColor = color.display();
+				div.setAttribute("style", `background-color: ${color.display()}; --l: ${l}; --c: ${c}; --h: ${h}`);
 				div.color = color;
 				colors.append(div);
 			}
@@ -50,18 +56,25 @@ function drawColors() {
 	computeTextColor();
 }
 
-contrast_algorithm.addEventListener("input", evt => {
-	computeTextColor();
-});
+function render(evt) {
+	if (!evt || evt.target === number_of_colors) {
+		number_of_colors.title = number_of_colors.value;
+		drawColors();
+	}
 
-number_of_colors.addEventListener("input", evt => {
-	number_of_colors.title = number_of_colors.value;
+	if (!evt || evt.target === contrast_algorithm) {
+		computeTextColor();
+	}
 
-	drawColors();
-})
+	if (!evt || evt.target === show_changes) {
+		root.classList.toggle("show-changes", show_changes.checked);
+	}
 
-show_changes.addEventListener("input", evt => {
-	root.classList.toggle("show-changes", show_changes.checked);
-})
+	if (!evt || evt.target === order_by) {
+		root.dataset.order = order_by.value;
+	}
+}
 
-drawColors();
+document.body.addEventListener("input", render);
+
+render();
