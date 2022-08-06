7db8febbd5e4c818e91ed87463595e42bbaf0899
Lea Verou <lea@verou.me>
2022-08-06 14:50:25 +0300

    Add contrast() function that works with all existing contrast algorithms
    - Case insensitive
    - Shows all available algos if none passed

--- src/contrast.js
+++ src/contrast.js
@@ -1,21 +1,28 @@
 import getColor from "./getColor.js";
-import {getLuminance} from "./luminance.js";
+// import defaults from "./defaults.js";
+import {isString} from "./util.js";
+import * as contrastAlgorithms from "./contrast/index.js";
 
-// WCAG 2.0 contrast https://www.w3.org/TR/WCAG20-TECHS/G18.html
-export default function contrast (color1, color2) {
-	color1 = getColor(color1);
-	color2 = getColor(color2);
-
-	let L1 = getLuminance(color1);
-	let L2 = getLuminance(color2);
-
-	if (L2 > L1) {
-		[L1, L2] = [L2, L1];
+export default function contrast (c1, c2, o = {}) {
+	if (isString(o)) {
+		o = {algorithm: o};
 	}
 
-	return (L1 + .05) / (L2 + .05);
-}
+	let {algorithm, ...rest} = o;
 
-export function register(Color) {
-	Color.defineFunction("contrast", contrast);
-}
+	if (!algorithm) {
+		let algorithms = Object.keys(contrastAlgorithms).map(a => a.replace(/^contrast/, "")).join(", ");
+		throw new TypeError(`contrast() function needs a contrast algorithm. Please specify one of: ${algorithms}`);
+	}
+
+	c1 = getColor(c1);
+	c2 = getColor(c2);
+
+	for (let a in contrastAlgorithms) {
+		if ("contrast" + algorithm.toLowerCase() === a.toLowerCase()) {
+			return contrastAlgorithms[a](c1, c2, rest);
+		}
+	}
+
+	throw new TypeError(`Unknown contrast algorithm: ${algorithm}`);
+};
