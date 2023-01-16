e7d58a0555af4118461e6e45acbf0dbbfa2f70ef
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2023-01-16 09:35:43 -0500

    [types] Add `value` to `setLuminance`

--- types/src/luminance.d.ts
+++ types/src/luminance.d.ts
@@ -2,6 +2,9 @@ import Color, { ColorObject } from "./color";
 
 export function getLuminance(color: Color | ColorObject): number;
 
-export function setLuminance(color: Color | ColorObject): void;
+export function setLuminance(
+    color: Color | ColorObject,
+    value: number | ((coord: number) => number),
+): void;
 
 export function register(color: typeof Color): void;
