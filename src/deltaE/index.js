da77a1b4382cfba68e435aeeff4cc04bae3dd389
Lea Verou <lea@verou.me>
2023-02-08 18:36:07 +0200

    Revert "Allow to extend the deltaE methods" (#283)

--- src/deltaE/index.js
+++ src/deltaE/index.js
@@ -4,12 +4,3 @@ export {default as deltaE2000} from "./deltaE2000.js";
 export {default as deltaEJz} from "./deltaEJz.js";
 export {default as deltaEITP} from "./deltaEITP.js";
 export {default as deltaEOK} from "./deltaEOK.js";
-
-export default {
-	deltaE76,
-	deltaECMC,
-	deltaE2000,
-	deltaEJz,
-	deltaEITP,
-	deltaEOK,
-};
