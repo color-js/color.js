f505c1b5230e1eabf5893f6a8eefb335936e55e0
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2023-09-14 13:50:43 -0400

    [types] Re-export some types at the top level (#327)
    * [types] Re-export some types at the top level
    
    Re-exports some potentially desirable types at the top level instead of
    requiring them to be manually imported from their individual files.
    
    * [types] Fix compatibility with `export *`
    
    * [types] Re-export options from `src/parse.d.ts`
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Re-export `White` from `src/adapt.d.ts`
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Include `PlainColorObject` in exports
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Fix typo in `SpaceOptions`
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Update tests from review (1/4)
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Update tests from review (2/4)
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Update tests from review (3/4)
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    * [types] Update tests from review (4/4)
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>
    
    ---------
    
    Co-authored-by: Jonny Gerig Meyer <jonny@oddbird.net>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,41 @@
 // Definitions by: Adam Thompson-Sharpe <https://github.com/MysteryBlokHed>
 // Minimum TypeScript Version: 4.1
 export { default } from "./dist/color";
+export type {
+	ColorConstructor,
+	ColorObject,
+	ColorTypes,
+	Coords,
+	DefineFunctionCode,
+	DefineFunctionOptions,
+	DefineFunctionHybrid,
+	PlainColorObject,
+	SpaceAccessor,
+	ToColorPrototype,
+} from "./src/color";
+
+export type { White } from "./src/adapt";
+
+export type { CAT } from "./src/CATs";
+
+export type { Display } from "./src/display";
+
+export type {
+	Range,
+	RangeOptions,
+	MixOptions,
+	StepsOptions,
+} from "./src/interpolation";
+
+export type { Options as ParseOptions } from "./src/parse";
+
+export type { RGBOptions } from "./src/rgbspace";
+
+export type { Options as SerializeOptions } from "./src/serialize";
+
+export type {
+	Format as SpaceFormat,
+	CoordMeta,
+	Ref,
+	Options as SpaceOptions,
+} from "./src/space";
---
+++ types/test/type-accessibility.ts
@@ -0,0 +1,37 @@
+// Testing whether types can be accessed from the main import
+
+import {
+	// Re-exported from src/color.d.ts
+	ColorConstructor,
+	ColorObject,
+	ColorTypes,
+	Coords,
+	DefineFunctionCode,
+	DefineFunctionOptions,
+	DefineFunctionHybrid,
+	PlainColorObject,
+	SpaceAccessor,
+	ToColorPrototype,
+	// Re-exported from src/adapt.d.ts
+	White,
+	// Re-exported from src/CATs.d.ts
+	CAT,
+	// Re-exported from src/display.d.ts
+	Display,
+	// Re-exported from src/interpolation.d.ts
+	Range,
+	RangeOptions,
+	MixOptions,
+	StepsOptions,
+	// Re-exported from src/parse.d.ts
+	ParseOptions,
+	// Re-exported from src/rgbspace.d.ts
+	RGBOptions,
+	// Re-exported from src/serialize.d.ts
+	SerializeOptions,
+	// Re-exported from src/space.d.ts
+	SpaceFormat,
+	CoordMeta,
+	Ref,
+	SpaceOptions,
+} from "colorjs.io";
