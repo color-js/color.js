66bd6d925fcfb3c8fc359da40e5293511078c4a5
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2023-09-13 20:47:53 -0400

    [types] Re-export some types at the top level
    Re-exports some potentially desirable types at the top level instead of
    requiring them to be manually imported from their individual files.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,26 @@
 // Definitions by: Adam Thompson-Sharpe <https://github.com/MysteryBlokHed>
 // Minimum TypeScript Version: 4.1
 export { default } from "./dist/color";
+export type * from "./src/color";
+
+export type { CAT } from "./src/CATs";
+
+export type { Display } from "./src/display";
+
+export type {
+	Range,
+	RangeOptions,
+	MixOptions,
+	StepsOptions,
+} from "./src/interpolation";
+
+export type { RGBOptions } from "./src/rgbspace";
+
+export type { Options as SerializeOptions } from "./src/serialize";
+
+export type {
+	Format as SpaceFormat,
+	CoordMeta,
+	Ref,
+	Options as SpacecOptions,
+} from "./src/space";
---
+++ types/test/type-accessibility.ts
@@ -0,0 +1,32 @@
+// Testing whether types can be accessed from the main import
+
+import {
+	// Re-exported from src/color.d.ts
+	ColorConstructor,
+	ColorObject,
+	ColorTypes,
+	Coords,
+	DefineFunctionCode,
+	DefineFunctionOptions,
+	DefineFunctionHybrid,
+	SpaceAccessor,
+	ToColorPrototype,
+	// Re-exported from src/CATs.d.ts
+	CAT,
+	// Re-exported from src/display.d.ts
+	Display,
+	// Re-exported from src/interpolation.d.ts
+	Range,
+	RangeOptions,
+	MixOptions,
+	StepsOptions,
+	// Re-exported from src/rgbspace.d.ts
+	RGBOptions,
+	// Re-exported from src/serialize.d.ts
+	SerializeOptions,
+	// Re-exported from src/space.d.ts
+	SpaceFormat,
+	CoordMeta,
+	Ref,
+	SpacecOptions,
+} from "colorjs.io";
