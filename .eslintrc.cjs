aac6e62085dc64db8c56813629c7b696c0a5c63a
Jonny Gerig Meyer <jonny@oddbird.net>
2023-11-29 12:56:58 -0500

    Revert "Remove eslint changes"
    This reverts commit 82d2ce9971b547fb518303bbff96dd628f4b1e67.

---
+++ .eslintrc.cjs
@@ -0,0 +1,97 @@
+module.exports = {
+	"root": true,
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": 6,
+		"sourceType": "module",
+		"ecmaFeatures": {
+			"impliedStrict": true,
+		},
+	},
+	"env": {
+		"browser": true,
+		"es6": true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/stylistic",
+		"plugin:@stylistic/disable-legacy",
+	],
+	"plugins": ["@typescript-eslint", "@stylistic"],
+	"rules": {
+		// Override recommended ESLint rules
+		// https://eslint.org/docs/latest/rules/
+		"no-cond-assign": 0,
+		"no-empty": 0,
+		"no-redeclare": 0,
+		"no-setter-return": 0,
+		"no-sparse-arrays": 0,
+
+		// Override recommended typescript-eslint rules
+		// https://typescript-eslint.io/rules/
+		"@typescript-eslint/no-empty-function": 0,
+		"@typescript-eslint/no-explicit-any": 0,
+		"@typescript-eslint/no-loss-of-precision": 0,
+		"@typescript-eslint/no-unused-vars": 0,
+
+		// Enable additional ESLint rules
+		"curly": 1,
+		"no-useless-call": 1,
+
+		// Enable ESLint Stylistic rules
+		// https://eslint.style/packages/default#rules
+		"@stylistic/arrow-spacing": 1,
+		"@stylistic/brace-style": [1, "stroustrup"],
+		"@stylistic/comma-spacing": 1,
+		"@stylistic/eol-last": 1,
+		"@stylistic/indent": [1, "tab", { "SwitchCase": 1, "outerIIFEBody": 0 }],
+		"@stylistic/keyword-spacing": 1,
+		"@stylistic/no-mixed-spaces-and-tabs": [1, "smart-tabs"],
+		"@stylistic/no-trailing-spaces": 1,
+		"@stylistic/quotes": [
+			1,
+			"double",
+			{ "avoidEscape": true, "allowTemplateLiterals": true },
+		],
+		"@stylistic/semi": 1,
+		"@stylistic/space-before-function-paren": 1,
+		"@stylistic/spaced-comment": [
+			1,
+			"always",
+			{ "block": { "exceptions": ["*"] } },
+		],
+	},
+	"overrides": [
+		{
+			"files": [
+				"apps/**/*",
+				"assets/**/*",
+				"get/**/*",
+				"notebook/**/*",
+				"scripts/**/*",
+				"tests/**/*",
+			],
+			"rules": {
+				// These directories reference implicit global variables
+				"no-undef": 0,
+			},
+		},
+		{
+			"files": ["*.cjs"],
+			"env": {
+				"browser": false,
+				"node": true,
+			},
+			"rules": {
+				"@typescript-eslint/no-var-requires": 0,
+			},
+		},
+		{
+			"files": ["types/test/**/*"],
+			"rules": {
+				"@typescript-eslint/ban-ts-comment": 0,
+			},
+		},
+	],
+};
