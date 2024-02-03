e36e183dd4f6d331d9ed9882985feb7e002d45ec
Lea Verou <lea@verou.me>
2024-02-03 01:43:02 -0500

    [spaces/oklch] Default to percentage for L
    Harmonizes with OkLab

--- src/spaces/oklch.js
+++ src/spaces/oklch.js
@@ -62,7 +62,7 @@ export default new ColorSpace({
 
 	formats: {
 		"oklch": {
-			coords: ["<number> | <percentage>", "<number> | <percentage>[0,1]", "<number> | <angle>"],
+			coords: ["<percentage> | <number>", "<number> | <percentage>[0,1]", "<number> | <angle>"],
 		}
 	}
 });
