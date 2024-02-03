d8d14f7d646e56a374876ef659927b1f1547efe1
Jonny Gerig Meyer <jonny@oddbird.net>
2024-02-03 13:04:30 -0600

    Add types for color space coord accessors. (#389)
    * Add types for color space coord accessors.
    
    * Sort coord accessors for minimal diffs
    
    * Fail CI if there are uncommitted changes
    
    * Test should fail CI
    
    * Fail with error msg
    
    * edit error msg
    
    * Fix build
    
    * Add file header
    
    * Generate space accessors in build step

---
+++ scripts/generate-space-accessor-types.js
@@ -0,0 +1,45 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import * as spaces from "../src/spaces/index-fn.js";
+
+const coords = new Set();
+const spaceIds = new Set();
+for (const spaceModule in spaces) {
+	const space = spaces[spaceModule];
+	spaceIds.add(space.id.replace(/-/g, "_"));
+	for (const alias of (space.aliases || [])) {
+		spaceIds.add(alias.replace(/-/g, "_"));
+	}
+	for (const coord in space.coords) {
+		coords.add(coord);
+	}
+}
+
+let template = `// DO NOT EDIT. This file is generated with \`npm run build:space-accessors\`.
+
+/** Proxy used for space accessors */
+export type SpaceAccessor = Record<string, number> & number[];
+
+declare class SpaceAccessors {`;
+for (const spaceId of Array.from(spaceIds).sort()) {
+	template += `\n\t${spaceId}: SpaceAccessor;`;
+}
+for (const coord of Array.from(coords).sort()) {
+	template += `\n\t${coord}: number;`;
+}
+template += `
+}
+
+export default SpaceAccessors;
+`;
+
+try {
+	const __dirname = path.dirname(fileURLToPath(import.meta.url));
+	const filePath = path.resolve(__dirname, "../types/src/space-coord-accessors.d.ts");
+	fs.writeFileSync(filePath, template);
+}
+catch (err) {
+	console.error(err);
+}
---
+++ types/src/space-coord-accessors.d.ts
@@ -0,0 +1,64 @@
+// DO NOT EDIT. This file is generated with `npm run build:space-accessors`.
+
+/** Proxy used for space accessors */
+export type SpaceAccessor = Record<string, number> & number[];
+
+declare class SpaceAccessors {
+	a98rgb: SpaceAccessor;
+	a98rgb_linear: SpaceAccessor;
+	acescc: SpaceAccessor;
+	acescg: SpaceAccessor;
+	cam16_jmh: SpaceAccessor;
+	hct: SpaceAccessor;
+	hsl: SpaceAccessor;
+	hsv: SpaceAccessor;
+	hwb: SpaceAccessor;
+	ictcp: SpaceAccessor;
+	jzazbz: SpaceAccessor;
+	jzczhz: SpaceAccessor;
+	lab: SpaceAccessor;
+	lab_d65: SpaceAccessor;
+	lch: SpaceAccessor;
+	oklab: SpaceAccessor;
+	oklch: SpaceAccessor;
+	p3: SpaceAccessor;
+	p3_linear: SpaceAccessor;
+	prophoto: SpaceAccessor;
+	prophoto_linear: SpaceAccessor;
+	rec2020: SpaceAccessor;
+	rec2020_linear: SpaceAccessor;
+	rec2100hlg: SpaceAccessor;
+	rec2100pq: SpaceAccessor;
+	srgb: SpaceAccessor;
+	srgb_linear: SpaceAccessor;
+	xyz: SpaceAccessor;
+	xyz_abs_d65: SpaceAccessor;
+	xyz_d50: SpaceAccessor;
+	xyz_d65: SpaceAccessor;
+	a: number;
+	az: number;
+	b: number;
+	bz: number;
+	c: number;
+	cp: number;
+	ct: number;
+	cz: number;
+	g: number;
+	h: number;
+	hz: number;
+	i: number;
+	j: number;
+	jz: number;
+	l: number;
+	m: number;
+	r: number;
+	s: number;
+	t: number;
+	v: number;
+	w: number;
+	x: number;
+	y: number;
+	z: number;
+}
+
+export default SpaceAccessors;
