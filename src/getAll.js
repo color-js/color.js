70dc9dc35a7857a282e51908b35c18d701ba6fac
Lea Verou <lea@verou.me>
2024-02-09 17:31:23 -0500

    `getAll()`: Make color space optional
    This is to set the stage for option 3 here: https://github.com/color-js/color.js/issues/388#issuecomment-1936678737 but also implements a performance improvement when the color passed *happens* to be in the same color space as the one specified.

--- src/getAll.js
+++ src/getAll.js
@@ -1,12 +1,17 @@
 import ColorSpace from "./space.js";
 
 /**
- * Get the coordinates of a color in another color space
- *
- * @param {string | ColorSpace} space
- * @returns {number[]}
+ * Get the coordinates of a color in any color space
+ * @param {Color} color
+ * @param {string | ColorSpace} [space = color.space] The color space to convert to. Defaults to the color's current space
+ * @returns {number[]} The color coordinates in the given color space
  */
 export default function getAll (color, space) {
+	if (!space || color.space.equals(space)) {
+		// No conversion needed
+		return color.coords.slice();
+	}
+
 	space = ColorSpace.get(space);
 	return space.from(color);
 }
