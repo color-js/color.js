b8590c892773de223ade6f61fa39baec53f50cbd
Jonny Gerig Meyer <jonny@oddbird.net>
2024-02-10 12:48:13 -0500

    Update types for getAll

--- types/test/getAll.ts
+++ types/test/getAll.ts
@@ -4,8 +4,7 @@ import sRGB from "colorjs.io/src/spaces/srgb";
 
 // @ts-expect-error
 getAll();
-// @ts-expect-error
-getAll(new Color("red"));
 
+getAll(new Color("red")); // $ExpectType [number, number, number]
 getAll(new Color("red"), "srgb"); // $ExpectType [number, number, number]
 getAll(new Color("red"), sRGB); // $ExpectType [number, number, number]
