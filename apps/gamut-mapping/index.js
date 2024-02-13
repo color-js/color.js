7985f5eff5ba05e16189f6679c37d51c2e5d4875
Lea Verou <lea@verou.me>
2024-02-13 14:52:07 -0500

    Gamut mapping experiment (WIP)

---
+++ apps/gamut-mapping/index.js
@@ -0,0 +1,41 @@
+import Color from "../../dist/color.js";
+
+globalThis.Color = Color;
+
+color_input.addEventListener("input", evt => {
+	console.log(evt)
+	let p3color = input_color.color.to("p3");
+	let p3Linear = input_color.color.to("p3-linear");
+	to_p3.color = p3color;
+	to_p3linear.color = p3Linear;
+
+	for (let cssColor of gamut_mapped.querySelectorAll("css-color")) {
+		let method = cssColor.dataset.method;
+		let color = p3color.clone();
+
+		if (color.inGamut("p3")) {
+			cssColor.color = color;
+			continue;
+		}
+
+		if (method === "scale") {
+			let deltas = p3Linear.coords.map(c => c - .5);
+			let distances = deltas.map(c => Math.abs(c));
+			let max = Math.max(...distances);
+			let factor = max / .5;
+
+			let mapped = deltas.map((delta, i) => {
+				let scaled = delta / factor;
+				return scaled + .5
+			});
+
+			cssColor.color = new Color("p3-linear", mapped).to("p3");
+		}
+		else {
+			cssColor.color = color.toGamut({ method });
+		}
+
+	}
+});
+
+color_input.dispatchEvent(new InputEvent("input"));
