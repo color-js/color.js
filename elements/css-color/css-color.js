03ea55cbea632bf671497885c6c5062ed263dc5d
Lea Verou <lea@verou.me>
2024-02-13 15:10:03 -0500

    <css-color> Minor refactor

--- elements/css-color/css-color.js
+++ elements/css-color/css-color.js
@@ -31,16 +31,14 @@ export default class CSSColor extends HTMLElement {
 	}
 
 	connectedCallback () {
+		// This should eventually be a custom state
+		this.#dom.wrapper.classList.toggle("static", this.#dom.input);
+
 		if (this.#dom.input) {
 			if (!this.#dom.input.incrementable) {
 				// Increment numbers by keyboard arrow keys
 				importIncrementable.then(Incrementable => new Incrementable(this.#dom.input));
 			}
-			this.#dom.wrapper.classList.remove("static");
-		}
-		else {
-			// This should eventually be a custom state
-			this.#dom.wrapper.classList.add("static");
 		}
 
 		this.#render();
