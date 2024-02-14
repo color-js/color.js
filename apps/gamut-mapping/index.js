aa56831d00e00307848f3da1d457c1b638a4ae01
Lea Verou <lea@verou.me>
2024-02-13 19:32:10 -0500

    [apps/gamut-mapping] Deltas

--- apps/gamut-mapping/index.js
+++ apps/gamut-mapping/index.js
@@ -3,6 +3,7 @@ import Color from "../../dist/color.js";
 globalThis.Color = Color;
 
 const favicon = document.querySelector('link[rel="shortcut icon"]');
+const lch = ["L", "C", "H"];
 
 css_color_input.addEventListener("input", evt => {
 	if (css_color.color === null) {
@@ -12,20 +13,35 @@ css_color_input.addEventListener("input", evt => {
 
 	colorUpdated()
 
-	let p3color = css_color.color.to("p3");
-	let p3Linear = css_color.color.to("p3-linear");
+	let inputColor = css_color.color;
+	let p3color = inputColor.to("p3");
+	let p3Linear = inputColor.to("p3-linear");
 
 	to_p3.color = p3color;
 	to_p3linear.color = p3Linear;
+
+	let deltasMap = new Map();
+	let minDeltas = [{value: Infinity}, {value: Infinity}, {value: Infinity}];
 
 	for (let cssColor of gamut_mapped.querySelectorAll("css-color")) {
 		let method = cssColor.dataset.method;
 		let color = p3color.clone();
 
+		let dd = cssColor.closest("dd");
+		let stats = dd.querySelector(".deltas");
+
+		if (!stats) {
+			dd.insertAdjacentHTML("beforeend", `<dl class="deltas"></dl>`);
+			stats = dd.querySelector(".deltas");
+		}
+
 		if (color.inGamut("p3")) {
 			cssColor.color = color;
+			stats.innerHTML = "";
 			continue;
 		}
+
+		let mappedColor;
 
 		if (method === "scale") {
 			let deltas = p3Linear.coords.map(c => c - .5);
@@ -38,11 +54,34 @@ css_color_input.addEventListener("input", evt => {
 				return scaled + .5
 			});
 
-			cssColor.color = new Color("p3-linear", mapped).to("p3");
+			mappedColor = new Color("p3-linear", mapped).to("p3");
 		}
 		else {
-			cssColor.color = color.toGamut({ method });
+			mappedColor = color.toGamut({ method });
 		}
+
+		cssColor.color = mappedColor;
+
+		// Show deltas
+		let mappedColorLCH = mappedColor.to("oklch").coords;
+		let deltas = inputColor.to("oklch").coords.map((c, i) => mappedColorLCH[i] - c);
+		deltasMap.set(cssColor, deltas);
+		stats.innerHTML = deltas.map((delta, i) => {
+			if (minDeltas[i].value > Math.abs(delta)) {
+				minDeltas[i] = {value: Math.abs(delta), stats, index: i};
+			}
+
+			delta = Color.util.toPrecision(delta, 2);
+			let cl = delta < 0? "negative" : delta > 0? "positive" : "zero";
+			return `<dt>Δ${ lch[i] }</dt><dd class="${ cl }">${ delta }</dd>`
+		}).join("");
+	}
+
+	// Find min deltas
+	for (let minDelta of minDeltas) {
+		// 0 -> 1, 1 -> 3, 2 -> 5
+		let dd = minDelta.stats.children[minDelta.index * 2 + 1];
+		dd.classList.add("min");
 	}
 });
 
