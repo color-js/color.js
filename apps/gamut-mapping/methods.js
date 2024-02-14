6e6a98d605a7bf1f34abfdffb0551dd91097696c
Lea Verou <lea@verou.me>
2024-02-13 22:01:06 -0500

    [apps/gamut-mapping]

--- apps/gamut-mapping/methods.js
+++ apps/gamut-mapping/methods.js
@@ -11,7 +11,7 @@ const methods = {
 	},
 	"scale-lh": {
 		label: "Scale LH",
-		description: "Like Scale, but sets L, H to those of the original color, and then performs Scale again.",
+		description: "Runs Scale, sets L, H to those of the original color, then runs Scale again.",
 		compute: (color) => {
 			let mappedColor = methods.scale.compute(color);
 			let lch = color.to("oklch").coords;
@@ -38,32 +38,32 @@ const methods = {
 			return new Color("p3-linear", mapped).to("p3");
 		}
 	},
-	"scale125": {
-		label: "Scale from 0.125",
-		description: "Using a midpoint of 0.125 (perceptual midpoint per Oklab), scale the color to fit within the linear P3 gamut using different scaling factors for values below and above the midpoint.",
-		compute: (color) => {
-			let p3Linear = color.to("p3-linear");
-			let deltas = p3Linear.coords.map(c => c - .125); /* in-gamut range is now -.125 to .875 */
-			let max = [Math.min(...deltas.filter(c => c < 0)), Math.max(...deltas.filter(c => c > 0))];
-			let factor = [max[0] / .125, max[1] / .875];
+	// "scale125": {
+	// 	label: "Scale from 0.125",
+	// 	description: "Using a midpoint of 0.125 (perceptual midpoint per Oklab), scale the color to fit within the linear P3 gamut using different scaling factors for values below and above the midpoint.",
+	// 	compute: (color) => {
+	// 		let p3Linear = color.to("p3-linear");
+	// 		let deltas = p3Linear.coords.map(c => c - .125); /* in-gamut range is now -.125 to .875 */
+	// 		let max = [Math.min(...deltas.filter(c => c < 0)), Math.max(...deltas.filter(c => c > 0))];
+	// 		let factor = [max[0] / .125, max[1] / .875];
 
-			let mapped = deltas.map((delta, i) => {
-				if (delta === 0) {
-					return .125;
-				}
-				else if (delta < 0) {
-					let scaled = delta / factor[0];
-					return scaled + .125;
-				}
-				else {
-					let scaled = delta / factor[1];
-					return scaled + .125;
-				}
-			});
+	// 		let mapped = deltas.map((delta, i) => {
+	// 			if (delta === 0) {
+	// 				return .125;
+	// 			}
+	// 			else if (delta < 0) {
+	// 				let scaled = delta / factor[0];
+	// 				return scaled + .125;
+	// 			}
+	// 			else {
+	// 				let scaled = delta / factor[1];
+	// 				return scaled + .125;
+	// 			}
+	// 		});
 
-			return new Color("p3-linear", mapped).to("p3");
-		}
-	},
+	// 		return new Color("p3-linear", mapped).to("p3");
+	// 	}
+	// },
 };
 
 export default methods;
