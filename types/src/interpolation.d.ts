83e81f52f4cba686379e3a656d894cf836299eea
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-02-16 07:31:15 -0500

    [types] Add docs for interpolation and parse functions

--- types/src/interpolation.d.ts
+++ types/src/interpolation.d.ts
@@ -9,13 +9,34 @@ export type Range = ((percentage: number) => Color) & {
 export function isRange (val: any): val is Range;
 
 export interface RangeOptions {
+	/**
+	 * The interpolation space.
+	 * Colors do not need to be in this space; they will be converted for interpolation
+	 * @see {@link <https://colorjs.io/docs/interpolation#ranges>}
+	 */
 	space?: string | ColorSpace | undefined;
 	outputSpace?: string | ColorSpace | undefined;
+	/**
+	 * Used to customize the progression and make in nonlinear
+	 * @example
+	 * let r = new Color("lch(50 50 0)").range("lch(90 50 20)");
+	 * Color.range(r, { progression: p => p ** 3 });
+	*/
 	progression?: ((percentage: number) => number) | undefined;
 	premultiplied?: boolean | undefined;
+	/**
+	 * Inspired by the
+	 * {@link https://drafts.csswg.org/css-color-5/#hue-adjuster hue-adjuster in CSS Color 5}.
+	 */
 	hue?: "longer" | "shorter" | "increasing" | "decreasing" | "raw" | undefined;
 }
 
+/**
+ * Creates a function that accepts a number and returns a color.
+ * For numbers in the range 0 to 1, the function interpolates;
+ * for numbers outside that range, the function extrapolates
+ * (and thus may not return the results you expect)
+ */
 export function range (range: Range, options?: RangeOptions): Range;
 export function range (
 	color1: ColorTypes,
@@ -25,6 +46,7 @@ export function range (
 
 export type MixOptions = RangeOptions;
 
+/** Create color mixtures in any desired proportion between two colors */
 export function mix (
 	color1: ColorTypes,
 	color2: ColorTypes,
@@ -38,12 +60,17 @@ export function mix (
 ): PlainColorObject;
 
 export interface StepsOptions extends RangeOptions {
+	/** max deltaE between consecutive steps */
 	maxDeltaE?: number | undefined;
+	/** @see {@link Methods} */
 	deltaEMethod?: Methods | undefined;
+	/** The minimum number of steps */
 	steps?: number | undefined;
+	/** The maximum number of steps */
 	maxSteps?: number | undefined;
 }
 
+/** Get an array of discrete steps */
 export function steps (color1: ColorTypes, color2: ColorTypes, options?: StepsOptions): PlainColorObject[];
 export function steps (range: Range, options?: StepsOptions): PlainColorObject[];
 
