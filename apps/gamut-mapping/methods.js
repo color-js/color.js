594d35aad2cb910ad7d5ed6144953395db6029c8
facelessuser <faceless.shop@gmail.com>
2024-02-19 10:09:45 -0700

    New gamut mapping approaches

--- apps/gamut-mapping/methods.js
+++ apps/gamut-mapping/methods.js
@@ -22,6 +22,115 @@ const methods = {
 			return methods.scale.compute(mappedColor);
 		},
 	},
+
+	"scale-lh-achromatic2": {
+		label: "Scale LH Achromatic 2",
+		description: (
+			"Like Scale LH Achromatic but applies 3 passes: 2X approaching gamut and final scaling into gamut."
+		),
+		compute: (color) => {
+			if (color.inGamut("p3")) {
+				return color.to("p3");
+			}
+			let method = methods["scale-achromatic"];
+			let mapColor = color.to("oklch");
+			let achroma = mapColor.clone().set("c", 0).to("p3-linear");
+			let gamutColor = mapColor.clone().to("p3-linear");
+			let lightness = mapColor.coords[0];
+
+			if (lightness >= 1) {
+				return new Color({ space: "xyz-d65", coords: WHITES["D65"] }).to("p3");
+			}
+			else if (lightness <= 0) {
+				return new Color({ space: "xyz-d65", coords: [0, 0, 0] }).to("p3");
+			}
+
+			let iterate = 2;
+			while (iterate--) {
+				let chroma = mapColor.coords[1];
+				method.scale(gamutColor, achroma);
+				let chroma2 = gamutColor.oklch.c;
+				mapColor.set('c', ((chroma2 - chroma) < 2) ? method.lerp(chroma, chroma2, 0.9) : chroma2 + 0.1);
+				let [red, green, blue] = mapColor.to("p3-linear").coords;
+				gamutColor.set({r: red, g: green, b: blue});
+			}
+
+			method.scale(gamutColor, achroma);
+			return gamutColor.to("p3");
+		}
+	},
+
+	"scale-lh-achromatic": {
+		label: "Scale LH Achromatic",
+		description: "Like Scale LH but uses Scale Achromatic and returns black or white if L<=0 or L>=1.",
+		compute: (color) => {
+			let method = methods["scale-achromatic"];
+			let mapColor = color.to("oklch");
+			let achroma = mapColor.clone().set("c", 0).to("p3-linear");
+			let gamutColor = mapColor.clone().to("p3-linear");
+			let lightness = mapColor.coords[0];
+
+			if (lightness >= 1) {
+				return new Color({ space: "xyz-d65", coords: WHITES["D65"] }).to("p3");
+			}
+			else if (lightness <= 0) {
+				return new Color({ space: "xyz-d65", coords: [0, 0, 0] }).to("p3");
+			}
+
+			method.scale(gamutColor, achroma);
+			gamutColor.set({
+				"oklch.l": mapColor.coords[0],
+				"oklch.h": mapColor.coords[2],
+			});
+			method.scale(gamutColor, achroma);
+			return gamutColor.to("p3");
+		},
+	},
+
+	"scale-achromatic": {
+		label: "Scale Achromatic",
+		"description": "Scale towards the achromatic version of the given color.",
+		compute: (color) => {
+			let mapColor = color.to("oklch");
+			let achroma = mapColor.clone().set("c", 0).to("p3-linear");
+			let gamutColor = mapColor.clone().to("p3-linear");
+			let lightness = mapColor.coords[0];
+			methods['scale-achromatic'].scale(gamutColor, achroma);
+			return gamutColor.to("p3");
+		},
+
+		lerp: (p0, p1, t) => {
+			return p0 + (p1 - p0) * t;
+		},
+
+		ilerp: (p0, p1, t) => {
+
+			let d = (p1 - p0);
+			return d !== 0 ? ((t - p0) / d) : 0;
+		},
+
+		scale: (color, achroma) => {
+			let deltas = []
+			let method = methods["scale-achromatic"];
+			color.coords.forEach((c, i) => {
+				if (c > 1) {
+					deltas.push(method.ilerp(c, achroma.coords[i], 1));
+				}
+				else if (c < 0) {
+					deltas.push(method.ilerp(c, achroma.coords[i], 0));
+				}
+			});
+
+			if (deltas.length > 0) {
+				const maxDelta = Math.max(...deltas);
+				color.set({
+					r: method.lerp(color.coords[0], achroma.coords[0], maxDelta),
+					g: method.lerp(color.coords[1], achroma.coords[1], maxDelta),
+					b: method.lerp(color.coords[2], achroma.coords[2], maxDelta)
+				});
+			}
+		}
+	},
 	"scale": {
 		label: "Scale",
 		description: "Using a midpoint of 0.5, scale the color to fit within the linear P3 gamut.",
