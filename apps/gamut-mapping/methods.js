9a6d8aa096e36095fe72ca5580325a000e7f2cae
facelessuser <faceless.shop@gmail.com>
2024-02-19 17:17:34 -0700

    Pull Scale LH Achromatic 2
    This should bake longer to work out some issues.

--- apps/gamut-mapping/methods.js
+++ apps/gamut-mapping/methods.js
@@ -24,43 +24,6 @@ const methods = {
 		},
 	},
 
-	"scale-lh-achromatic2": {
-		label: "Scale LH Achromatic 2",
-		description: (
-			"Like Scale LH Achromatic but applies 3 passes: 2X approaching gamut and final scaling into gamut."
-		),
-		compute: (color) => {
-			if (color.inGamut("p3")) {
-				return color.to("p3");
-			}
-			let method = methods["scale-achromatic"];
-			let mapColor = color.to("oklch");
-			let achroma = mapColor.clone().set("c", 0).to("p3-linear");
-			let gamutColor = mapColor.clone().to("p3-linear");
-			let lightness = mapColor.coords[0];
-
-			if (lightness >= 1) {
-				return new Color({ space: "xyz-d65", coords: WHITES["D65"] }).to("p3");
-			}
-			else if (lightness <= 0) {
-				return new Color({ space: "xyz-d65", coords: [0, 0, 0] }).to("p3");
-			}
-
-			let iterate = 2;
-			while (iterate--) {
-				let chroma = mapColor.coords[1];
-				method.scale(gamutColor, achroma);
-				let chroma2 = gamutColor.oklch.c;
-				mapColor.set("c", ((chroma2 - chroma) < 2) ? method.lerp(chroma, chroma2, 0.85) : chroma2 + 0.1);
-				let [red, green, blue] = mapColor.to("p3-linear").coords;
-				gamutColor.set({r: red, g: green, b: blue});
-			}
-
-			method.scale(gamutColor, achroma);
-			return gamutColor.to("p3");
-		},
-	},
-
 	"scale-lh-achromatic": {
 		label: "Scale LH Achromatic",
 		description: "Like Scale LH but uses Scale Achromatic and returns black or white if L<=0 or L>=1.",
