8ae3b44df7f09b2c1253e94a6b340dda10199bc7
Alexey Ardov <ardovalexey@gmail.com>
2024-02-27 14:43:28 +0100

    [apps/gamut-mapping] Add edge seeker algorithm (#448)
    * Add edge seeker algorithm
    
    * add hue normalization

--- apps/gamut-mapping/methods.js
+++ apps/gamut-mapping/methods.js
@@ -1,6 +1,14 @@
 import Color from "../../dist/color.js";
 import { WHITES } from "../../src/adapt.js";
 import * as util from "../../src/util.js";
+import { makeEdgeSeeker } from "./edge-seeker/makeEdgeSeeker.js";
+
+// Make a function to get the maximum chroma for a given lightness and hue
+// Lookup table is created once and reused
+const p3EdgeSeeker = makeEdgeSeeker((r, g, b) => {
+	const [l, c, h = 0] = new Color("p3", [r, g, b]).to("oklch").coords;
+	return {l, c, h};
+});
 
 const methods = {
 	"clip": {
@@ -212,6 +220,25 @@ const methods = {
 				];
 			}
 			return [0, []];
+		},
+	},
+	"edge-seeker": {
+		label: "Edge Seeker",
+		description: "Using a LUT to detect edges of the gamut and reduce chroma accordingly.",
+		compute: (color) => {
+			let [l, c, h] = color.to("oklch").coords;
+			if (l <= 0) {
+				return new Color("oklch", [0, 0, h]);
+			}
+			if (l >= 1) {
+				return new Color("oklch", [1, 0, h]);
+			}
+			let maxChroma = p3EdgeSeeker(l, h || 0);
+			if (c > maxChroma) {
+				c = maxChroma;
+			}
+			// At this point it is safe to clip the values
+			return new Color("oklch", [l, c, h]).toGamut({space: "p3", method: "clip"});
 		},
 	},
 	// "scale125": {
