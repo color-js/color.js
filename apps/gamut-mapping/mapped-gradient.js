10cfe1be09ae2713b6b79923871099988e63b6a4
James Stuckey Weber <jamesnw@gmail.com>
2024-03-05 11:59:43 -0500

    apps/gamut-mapping Add gradient tool view

---
+++ apps/gamut-mapping/mapped-gradient.js
@@ -0,0 +1,66 @@
+import methods from "./methods.js";
+
+export default {
+	props: {
+		method: String | Object,
+		steps: Array,
+	},
+
+	data () {
+		return {
+			time: 0,
+			mappedSteps: [],
+		};
+	},
+
+	computed: {
+		name () {
+			return methods[this.method]?.label || "None";
+		},
+	},
+
+	methods: {
+		mapSteps () {
+			const start = performance.now();
+			let steps = this.steps.map(step => {
+				let mappedColor;
+				if (this.method === "none") {
+					return step;
+				}
+				if (methods[this.method].compute) {
+					mappedColor = methods[this.method].compute(step);
+				}
+				else {
+					mappedColor = step.clone().toGamut({ space: "p3", method: this.method });
+				}
+				return mappedColor;
+			});
+			this.time = Color.util.toPrecision(performance.now() - start, 4);
+			this.mappedSteps = steps;
+		},
+	},
+
+	watch: {
+		steps: {
+			handler () {
+				this.mapSteps();
+			},
+			immediate: true,
+		},
+	},
+
+	compilerOptions: {
+		isCustomElement (tag) {
+			return tag === "css-color";
+		},
+	},
+
+	template: `
+	<div :style="style" class="mapped-gradient">
+		<div class="info"><strong>{{ name }}</strong> {{time}}ms</div>
+		<div class="gradient" :title="name">
+			<div v-for="step in mappedSteps" :style="{'--step-color': step}" :title="step"></div>
+		</div>
+	</div>
+		`,
+};
