58108b468256dd3af2938ffad07f614ffc662301
Chris Lilley <chris@w3.org>
2024-03-14 12:56:10 -0400

    lint

--- src/deltaE/deltaEOK2.js
+++ src/deltaE/deltaEOK2.js
@@ -2,9 +2,9 @@
 // than the simple 1976 Euclidean distance in CIE Lab
 // The Oklab a and b axes are scaled relative to the L axis, for better uniformity
 // Björn Ottosson said:
-// "I've recently done some tests with color distance datasets as implemented 
-// in Colorio and on both the Combvd dataset and the OSA-UCS dataset a 
-// scale factor of slightly more than 2 for a and b would give the best results 
+// "I've recently done some tests with color distance datasets as implemented
+// in Colorio and on both the Combvd dataset and the OSA-UCS dataset a
+// scale factor of slightly more than 2 for a and b would give the best results
 // (2.016 works best for Combvd and 2.045 for the OSA-UCS dataset)."
 // https://github.com/w3c/csswg-drafts/issues/6642#issuecomment-945714988
 
