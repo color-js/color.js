6a6e19cb06496c4e6f6be008664afd9a28c4716e
Lea Verou <lea@verou.me>
2024-03-23 15:23:02 -0400

    More default title fixing

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -9,14 +9,21 @@ export default {
 
 		// Title must appear in first 1000 chars
 		let content = data.page.rawInput.slice(0, 1000);
+		let ret;
 
 		if (ext === "md") {
 			// First heading
-			return content.match(/^#+\s+(.*)/m)?.[1];
+			ret = content.match(/^#+\s+(.*)/m)?.[1];
 		}
 		else if (ext === "njk") {
 			// First level 1 heading
-			return content.match(/<h1>(.*)<\/h1>/)?.[1]?.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+			ret = content.match(/<h1>(.*)<\/h1>/)?.[1];
 		}
+
+		if (ret) {
+			ret = ret.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+		}
+
+		return ret;
 	},
 };
