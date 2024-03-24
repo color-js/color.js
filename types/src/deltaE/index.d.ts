0ca996a5625fd1da8135c734e818b7b8123b14a6
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-03-24 16:39:35 -0400

    [types] Add more module documentation (#498)

--- types/src/deltaE/index.d.ts
+++ types/src/deltaE/index.d.ts
@@ -1,3 +1,7 @@
+/**
+ * @packageDocumentation
+ * This module defines all the builtin deltaE methods.
+ */
 export { default as deltaE76 } from "./deltaE76.js";
 export { default as deltaECMC } from "./deltaECMC.js";
 export { default as deltaE2000 } from "./deltaE2000.js";
@@ -9,6 +13,10 @@ export { default as deltaEHCT } from "./deltaEHCT.js";
 declare const deltaEMethods: Omit<typeof import("./index.js"), "default">;
 export default deltaEMethods;
 
+/**
+ * A type including all the names of the builtin deltaE methods,
+ * generated from the names of the deltaE functions.
+ */
 export type Methods = keyof typeof deltaEMethods extends `deltaE${infer Method}`
 	? Method
 	: string;
