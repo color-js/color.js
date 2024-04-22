d815abde9352bd8e64a6cbedd8ccea6be53f8346
Lea Verou <lea@verou.me>
2024-04-22 19:41:08 -0400

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
-let $ = Bliss;
-let $$ = $.$;
-
 import "./prism.js";
 import "https://blissfuljs.com/bliss.shy.js";
 import "https://live.prismjs.com/src/prism-live.mjs?load=javascript";
 import "../../notebook/color-notebook.js";
 import "./colors.js";
 import { styleCallouts } from "./enhance.js";
+
+let $ = Bliss;
+let $$ = $.$;
 
 if (location.pathname.indexOf("/docs/") > -1 && window.toc) {
 	import("./docs.js");
