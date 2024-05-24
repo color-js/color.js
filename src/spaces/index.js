719ab1b0d5c114d356bf08c088f33ee122507d3a
Lea Verou <lea@verou.me>
2024-05-24 17:34:06 -0400

    Export ColorSpace from spaces/index.js

--- src/spaces/index.js
+++ src/spaces/index.js
@@ -1,8 +1,9 @@
 import ColorSpace from "../space.js";
 import * as spaces from "./index-fn.js";
 
-export * as spaces from "./index-fn.js";
-
 for (let key of Object.keys(spaces)) {
 	ColorSpace.register(spaces[key]);
 }
+
+export * as spaces from "./index-fn.js";
+export default ColorSpace;
