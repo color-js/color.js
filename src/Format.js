5849f32e58133c60eb88265c3283e5d56699f0a7
Lea Verou <lea@verou.me>
2024-05-27 19:59:20 -0400

    Split Type out into a separate class

--- src/Format.js
+++ src/Format.js
@@ -1,17 +1,7 @@
-import { serializeNumber, mapRange, isNone } from "./util.js";
+import { isNone } from "./util.js";
+import Type from "./Type.js";
 
 let instance = Symbol("instance");
-
-const outputByType = {
-	"<percentage>": {
-		toRange: [0, 100],
-		suffix: "%"
-	},
-	"<angle>": {
-		toRange: [0, 360],
-		suffix: "deg"
-	},
-}
 
 /**
  * @class Format
@@ -43,7 +33,7 @@ export default class Format {
 				}
 
 				return ret;
-			})
+			});
 		}
 
 		this.coords = this.coords.map((types, i) => {
@@ -107,96 +97,5 @@ export default class Format {
 		}
 
 		return new Format(format, ...args);
-	}
-}
-
-class Type {
-	constructor (type, coordMeta) {
-		if (typeof type === "object") {
-			this.coordMeta = type;
-		}
-
-		if (coordMeta) {
-			this.coordRange = coordMeta.range ?? coordMeta.refRange;
-		}
-
-		if (typeof type === "string") {
-			let params = type.trim().match(/^(?<type><[a-z]+>)(\[(?<min>-?[.\d]+),\s*(?<max>-?[.\d]+)\])?$/);
-
-			if (!params) {
-				throw new TypeError(`Cannot parse ${type} as a type definition.`);
-			}
-
-			this.type = params.groups.type;
-			let {min, max} = params.groups;
-
-			if (min || max) {
-				this.range = [+min, +max];
-			}
-		}
-
-		if (!this.range) {
-			if (this.type === "<percentage>") {
-				this.range = [-1, 1];
-			}
-			else if (this.type === "<angle>") {
-				this.range = [0, 360];
-			}
-		}
-	}
-
-	get unit () {
-		if (this.type === "<percentage>") {
-			return "%";
-		}
-		else if (this.type === "<angle>") {
-			return "deg";
-		}
-
-		return "";
-	}
-
-	/**
-	 * Map a number to the internal representation
-	 * @param {number} number
-	 * @returns
-	 */
-	resolve (number) {
-		if (this.type === "<angle>") {
-			return number;
-		}
-
-		let fromRange = this.range;
-		let toRange = this.coordRange;
-
-		if (this.type === "<percentage>") {
-			toRange ??= [-1, 1];
-		}
-
-		return mapRange(fromRange, toRange, number);
-	}
-
-	/**
-	 * Serialize a number from the internal representation to a string
-	 * @param {number} number
-	 * @param {number} [precision]
-	 * @returns
-	 */
-	serialize (number, precision) {
-		let toRange = this.type === "<percentage>" ? [-100, 100] : this.range;
-		let unit = this.unit;
-
-		number = mapRange(this.coordRange, toRange, number);
-		number = serializeNumber(number, {unit, precision});
-
-		return number;
-	}
-
-	static get (type, ...args) {
-		if (type instanceof Type) {
-			return type;
-		}
-
-		return new Type(type, ...args);
 	}
 }
