90f61ae6f18a0c643137292e80e2d0a07f09f570
Lea Verou <lea@verou.me>
2024-05-31 11:31:48 -0400

    Handle hue angles
    - `hue` option to customize how hues are handled
    - `shorter` by default

--- src/deltas.js
+++ src/deltas.js
@@ -1,28 +1,36 @@
 import getColor from "./getColor.js";
 import ColorSpace from "./space.js";
 import to from "./to.js";
+import { adjust } from "./angles.js";
 
 /**
  * Get color differences per-component, on any color space
  * @param {Color} c1
  * @param {Color} c2
  * @param {object} options
- * @param {string} [options.space=c1.space] - The color space to use for the delta calculation. Defaults to the color space of the first color.
+ * @param {string | ColorSpace} [options.space=c1.space] - The color space to use for the delta calculation. Defaults to the color space of the first color.
+ * @param {string} [options.hue="shorter"] - How to handle hue differences. Same as hue interpolation option.
  * @returns {number[]} - An array of differences per component.
  * 		If one of the components is none, the difference will be 0.
  * 		If both components are none, the difference will be none.
  */
-export default function deltas (c1, c2, {space} = {}) {
+export default function deltas (c1, c2, {space, hue = "shorter"} = {}) {
 	c1 = getColor(c1);
 	space ||= c1.space;
+	space = ColorSpace.get(space);
+	let spaceCoords = Object.values(space.coords);
 
 	[c1, c2] = [c1, c2].map(c => to(c, space));
 	let [coords1, coords2] = [c1, c2].map(c => c.coords);
 
 	let coords = coords1.map((coord1, i) => {
+		let coordMeta = spaceCoords[i];
 		let coord2 = coords2[i];
 
-		if (coord1 === null || coord2 === null) {
+		if (coordMeta.type === "angle") {
+			[coord1, coord2] = adjust(hue, [coord1, coord2]);
+		}
+		else if (coord1 === null || coord2 === null) {
 			// Handle none
 			return coord1 === coord2 ? null : 0;
 		}
