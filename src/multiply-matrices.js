7d6594c2af121ee23c3f5c7fdbcadc8becd10290
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-06-07 16:25:52 +0200

    Update multiply-matrices.js

--- src/multiply-matrices.js
+++ src/multiply-matrices.js
@@ -14,37 +14,35 @@ export default function multiplyMatrices (A, B) {
 
 	if (!Array.isArray(B[0])) {
 		// B is vector, convert to [[a], [b], [c], ...]]
-		B = B.map((x) => [x]);
+		B = B.map(x => [x]);
 	}
 
 	let p = B[0].length;
-	let B_cols = B[0].map((_, i) => B.map((x) => x[i])); // transpose B
-	let product = A.map((row) =>
-		B_cols.map((col) => {
-			let ret = 0;
+	let B_cols = B[0].map((_, i) => B.map(x => x[i])); // transpose B
+	let product = A.map(row => B_cols.map(col => {
+		let ret = 0;
 
-			if (!Array.isArray(row)) {
-				for (let c of col) {
-					ret += row * c;
-				}
-
-				return ret;
-			}
-
-			for (let i = 0; i < row.length; i++) {
-				ret += row[i] * (col[i] || 0);
+		if (!Array.isArray(row)) {
+			for (let c of col) {
+				ret += row * c;
 			}
 
 			return ret;
-		}),
-	);
+		}
+
+		for (let i = 0; i < row.length; i++) {
+			ret += row[i] * (col[i] || 0);
+		}
+
+		return ret;
+	}));
 
 	if (m === 1) {
 		product = product[0]; // Avoid [[a, b, c, ...]]
 	}
 
 	if (p === 1) {
-		return product.map((x) => x[0]); // Avoid [[a], [b], [c], ...]]
+		return product.map(x => x[0]); // Avoid [[a], [b], [c], ...]]
 	}
 
 	return product;
