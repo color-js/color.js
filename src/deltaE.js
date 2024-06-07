d7267e5b1a9676c2b89cf467c079a98373d14291
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-06-07 16:05:21 +0200

    Update deltaE.js

--- src/deltaE.js
+++ src/deltaE.js
@@ -1,5 +1,5 @@
 import defaults from "./defaults.js";
-import { isString } from "./util.js";
+import {isString} from "./util.js";
 import deltaEMethods from "./deltaE/index.js";
 
 // Type "imports"
@@ -17,10 +17,10 @@ import deltaEMethods from "./deltaE/index.js";
  */
 export default function deltaE (c1, c2, o = {}) {
 	if (isString(o)) {
-		o = { method: o };
+		o = {method: o};
 	}
 
-	let { method = defaults.deltaE, ...rest } = o;
+	let {method = defaults.deltaE, ...rest} = o;
 
 	for (let m in deltaEMethods) {
 		if ("deltae" + method.toLowerCase() === m.toLowerCase()) {
