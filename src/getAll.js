b45fb8fd73d5b40ffb57d34fab5f022e79f221c7
Lloyd Kupchanko <lloyd@lloydk.ca>
2024-06-10 22:56:40 -0600

    Fix getAll() types
    Changes the space param to be optional.

--- src/getAll.js
+++ src/getAll.js
@@ -7,7 +7,7 @@ import getColor from "./getColor.js";
 /**
  * Get the coordinates of a color in any color space
  * @param {ColorTypes} color
- * @param {string | ColorSpace} space The color space to convert to. Defaults to the color's current space
+ * @param {string | ColorSpace} [space] The color space to convert to. Defaults to the color's current space
  * @returns {Coords} The color coordinates in the given color space
  */
 export default function getAll (color, space) {
