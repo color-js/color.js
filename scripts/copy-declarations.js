573b64d46a2e64a3d7c2d51e79e7cbaeeea055bf
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-06-24 19:02:23 -0400

    fix: Add missing `copy-declarations.js` script

---
+++ scripts/copy-declarations.js
@@ -0,0 +1,31 @@
+// Copy manually-written `.d.ts` files from `src/` to `types/src/`
+import { copyFileSync } from "node:fs";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const SOURCE = "../src/";
+const DEST = "../types/src/";
+
+/**
+ * `.d.ts` files to copy.
+ * Paths written relative to the `src/` directory
+ */
+const TO_COPY = [
+	"color.d.ts",
+	"ColorSpace.d.ts",
+	"hooks.d.ts",
+	"index.d.ts",
+	"space-coord-accessors.d.ts",
+	"space.d.ts",
+	"types.d.ts",
+];
+
+/** Script file directory */
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+for (const file of TO_COPY) {
+	copyFileSync(
+		resolve(__dirname, SOURCE, file),
+		resolve(__dirname, DEST, file),
+	);
+}
