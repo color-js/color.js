184363a66c43aadb78bd012ad0f1dbd7cc711a95
Lloyd Kupchanko <lloyd@lloydk.ca>
2024-06-27 15:04:23 -0600

    Fix remaining type test failures (#569)
    * Remove obsolete util type tests
    
    * Fix and add additional contrast type tests
    
    The default value of the `o` parameter was causing the generated
    type files to declare the `o` parameter as optional. This was
    causing a failure in the contrast type tests.
    
    Added additional tests for the contrast algorithms and functions
    on the Color class.
    
    * Fix and add additional deltaE type tests
    
    * Fix usage of deltaE Methods type

--- types/test/contrast.ts
+++ types/test/contrast.ts
@@ -12,8 +12,27 @@ contrast(c1);
 // @ts-expect-error
 contrast(c1, c2);
 
+// @ts-expect-error
+contrast(c1, c2, "abc");
+
+// @ts-expect-error
+contrast(c1, c2, { algorithm: "abc" });
+
 contrast(c1, c2, "APCA"); // $ExpectType number
 contrast(c1, c2, { algorithm: "APCA" }); // $ExpectType number
 
 // Make sure that contrast methods are properly re-exported
 contrastAPCA("red", "blue"); // $ExpectType number
+
+// @ts-expect-error
+c1.contrast(c2);
+
+// @ts-expect-error
+c1.contrast(c2, "abc");
+
+// @ts-expect-error
+c1.contrast(c2, { algorithm: "abc" });
+
+c1.contrast(c2, "APCA"); // $ExpectType number
+c1.contrast(c2, { algorithm: "APCA" }); // $ExpectType number
+c1.contrastAPCA(c2); // $ExpectType number
