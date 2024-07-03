51e86b9cb1c0cd12176a55f081a1e4d42eb73547
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2024-07-03 14:26:40 -0400

    fix(types): Suggestions from review
    Co-Authored-By: Lloyd Kupchanko <lloyd@lloydk.ca>

--- src/clone.js
+++ src/clone.js
@@ -1,16 +1,16 @@
 // Type "imports"
 /** @typedef {import("./color.js").default} Color */
+/** @typedef {import("./types.js").Coords} Coords */
 /** @typedef {import("./types.js").PlainColorObject} PlainColorObject */
 
 /**
- *
- * @param {Color | PlainColorObject} color
+ * @param {PlainColorObject} color
  * @returns {PlainColorObject}
  */
 export default function clone (color) {
 	return {
 		space: color.space,
-		coords: /** @type {[number, number, number]} */ (color.coords.slice()),
+		coords: /** @type {Coords} */ (color.coords.slice()),
 		alpha: color.alpha,
 	};
 }
