1f3bb9d4a6c2df839a97011873de4174afa60d5a
Lloyd Kupchanko <lloyd@lloydk.ca>
2024-07-15 13:51:01 -0600

    Fix type errors for okhsl and okhsv

--- src/spaces/okhsl.js
+++ src/spaces/okhsl.js
@@ -158,7 +158,7 @@ function getStMid (a, b) {
 
 /**
  * @param {number[]} lab
- * @param {number[]} lmsToRgb
+ * @param {number[][]} lmsToRgb
  */
 export function oklabToLinearRGB (lab, lmsToRgb) {
 	// Convert from Oklab to linear RGB.
@@ -175,8 +175,8 @@ export function oklabToLinearRGB (lab, lmsToRgb) {
 }
 
 /**
- * @param {[number, number]} a
- * @param {[number, number]} b
+ * @param {number} a
+ * @param {number} b
  * @param {number[][]} lmsToRgb
  * @param {number[][]} okCoeff
  * @returns {[number, number]}
