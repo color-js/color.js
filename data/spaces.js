ea1b40644fd4763be81cb9e6526d137d5a43e081
Dmitry Sharabin <dmitrysharabin@gmail.com>
2024-10-04 23:30:29 +0200

    Make `space.js` work both in Node.js and client-side (#600)
    * Fix typo
    
    * Make it work both in Node.js and client-side
    
    * No need to use relative URL

--- data/spaces.js
+++ data/spaces.js
@@ -5,7 +5,18 @@ let whitePoints = Object.entries(WHITES);
 
 // 11ty chokes on this
 // import modules from "./modules.json" with { type: "json" };
-const modules = await (await fetch(new URL("data/modules.json", import.meta.url)).json());
+let modules;
+let url = new URL("modules.json", import.meta.url);
+if (url.protocol === "file:") {
+	// We are in Node.js
+	// fetch() doesn't work with file:// URLs
+	const { readFileSync } = await import("fs");
+
+	modules = JSON.parse(readFileSync(url));
+}
+else {
+	modules = await (await fetch(url)).json();
+}
 
 let spaces = {};
 
